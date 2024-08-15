53870c0c06d4703eb15846a4b2615e594c30b6ae
Jordan Pittman <jordan@cryptica.me>
2024-08-15 18:09:17 -0400

    Initial Commit

---
+++ VZAutomation/VZAutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VZAutomationApp.swift
+//  VZAutomation
+//
+//  Created by Jordan Pittman on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct VZAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
